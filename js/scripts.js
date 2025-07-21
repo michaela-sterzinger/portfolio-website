838aebbab3d4f8a0a971663aa12708a123264be8
michaela-sterzinger <sterzinger.michaela@swm.de>
2025-07-21 11:43:45 +0200

    add javascript and responsive navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hello world")
+
+function menuToggle() {
+    var x = document.getElementById('mynavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } 
+    
+    else {
+        x.className = 'navtoggle';
+    }
+}
